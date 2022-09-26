4c024c21424033e1b0093d507f04a02a03f34a42
Scriper.irk <scriper.irk@gmail.com>
2022-09-26 22:13:01 +0800

    Start_1

--- src/test/java/ru/netology/web/page/LoginPage.java
+++ src/test/java/ru/netology/web/page/LoginPage.java
@@ -4,7 +4,7 @@ import ru.netology.web.data.DataHelper;
 
 import static com.codeborne.selenide.Selenide.$;
 
-public class LoginPageV1 {
+public class LoginPage {
   public VerificationPage validLogin(DataHelper.AuthInfo info) {
     $("[data-test-id=login] input").setValue(info.getLogin());
     $("[data-test-id=password] input").setValue(info.getPassword());
