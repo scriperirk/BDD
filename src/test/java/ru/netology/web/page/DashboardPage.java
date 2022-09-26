6897a5ff8162da436940a3b5ae7da9ef92ad95dd
Scriper.irk <scriper.irk@gmail.com>
2022-09-26 22:29:58 +0800

    Step_2

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -1,14 +1,35 @@
 package ru.netology.web.page;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import lombok.val;
 
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
-  private SelenideElement heading = $("[data-test-id=dashboard]");
+  private final SelenideElement heading = $("[data-test-id=dashboard]");
+  private final ElementsCollection deposit1 = $$("[data-test-id=action-deposit]").first(1);
+  private final ElementsCollection deposit2 = $$("[data-test-id=action-deposit]").last(1);
+  private final ElementsCollection cards = $$(".list__item");
+  private final String balanceStart = "баланс: ";
+  private final String balanceFinish = " р.";
 
   public DashboardPage() {
-    heading.shouldBe(visible);
+    heading.shouldBe(Condition.visible);
+    // убеждаемся, что перешли на третью страницу
   }
+
+  public int getCardBalance(int cardIndex) {
+    val text = cards.get(cardIndex).text();
+    return extractBalance(text);
+  }
+  private int extractBalance(String text) {
+    val start = text.indexOf(balanceStart);
+    val finish = text.indexOf(balanceFinish);
+    val value = text.substring(start + balanceStart.length(), finish);
+    return Integer.parseInt(value);
+  }
+
 }
